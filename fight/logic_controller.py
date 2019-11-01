525429e2c1b7906662e9790d37605f1c8b11cdfd
kewenguang <2860881669@qq.com>
2019-11-01 23:39:30 +0800

    鸣人佐助音效都好了

--- fight/logic_controller.py
+++ fight/logic_controller.py
@@ -38,6 +38,9 @@ class Controller():
         
     def set_mixer(self, mixer):
         self.mixer = mixer
+        self.channel2 = mixer.Channel(2)
+        self.channel3 = mixer.Channel(3)
+        self.channel4 = mixer.Channel(4)
     
     def play_music(self, url):
         self.mixer.music.load('../assets/bgm/' + url)
@@ -45,8 +48,16 @@ class Controller():
         
     def play_sound(self, url):
         sound = self.mixer.Sound('../assets/bgm/' + url)
-        sound.play()
+        self.channel2.play(sound)
         
+    def play_sound_by_channel3(self, url):
+        sound = self.mixer.Sound('../assets/bgm/' + url)
+        self.channel3.play(sound)
+        
+    def play_sound_queue(self, url):
+        sound = self.mixer.Sound('../assets/bgm/' + url)
+        self.channel3.queue(sound)
+
     #鸣人特效##############################################################################################
     def sub_insert_action_num(self):
         self.insert_action_num = self.insert_action_num - 1
@@ -63,9 +74,9 @@ class Controller():
         
     
     def cast_voice_luo_xuan_wan(self, image_index):
-        if image_index == 17:
-            self.play_music('打中别人.mp3')
-        elif image_index == 19:
+        if image_index == 15:
+            self.play_sound('打中别人.wav')
+        elif image_index == 23:
             self.play_sound('影分身消失.wav')
             
     
@@ -73,7 +84,7 @@ class Controller():
         if image_index == 2:
             self.play_sound('通灵出来的声音.wav')
         elif image_index == 5:
-            self.play_music('螺旋丸声音.mp3')
+            self.play_sound('螺旋丸声音.wav')
         elif image_index == 10:
             self.play_sound('影分身消失.wav')
         elif image_index == 14:
@@ -109,7 +120,7 @@ class Controller():
         if image_index % 5 == 0 and image_index < 62 and image_index > 9:
             self.saske_style.change_to_houyang()
         if image_index != 0 and image_index < 51 and image_index%5 == 0:
-            self.play_music('yun_beng.mp3')
+            self.play_sound('yun_beng.wav')
         
     def end_update_fenshen(self):
         print("6个放完了，接下来是多重影分身")
@@ -189,6 +200,8 @@ class Controller():
             self.saske_xuzuo_jian.remove_current_sprite_from_sprite_group()
             self.naruto_style.change_to_status('death')
             self.saske_xuzuo.character['saske/须左'].clear_update_function()
+            #self.play_sound('打中别人.wav') #须左结束的时候也放一下音效
+            self.play_sound('须左消失.wav')
             #self.saske_xuzuo_jian.character['saske/须左的箭'].image_index = 14
         else:
             self.saske_xuzuo_jian.character['saske/须左的箭'].set_left_padding(left_padding - 120)
@@ -199,6 +212,10 @@ class Controller():
             self.saske_xuzuo_jian.set_left_padding(self.saske_style.get_left_padding() - 200)
             self.saske_xuzuo_jian.set_top_padding(self.saske_style.get_top_padding() - 30)
             self.saske_xuzuo_jian.character['saske/须左的箭'].append_update_function(self.xuzuo_jian_update)
+        elif image_index == 8:
+            self.play_sound('搭箭.wav')
+        elif image_index == 11:
+            self.play_sound_queue('射出箭.wav')
         elif image_index == 14:
             self.saske_xuzuo.character['saske/须左'].image_index = 13
             
@@ -209,9 +226,11 @@ class Controller():
         self.saske_xuzuo.set_top_padding(self.saske_style.get_top_padding() + 20)
         self.saske_xuzuo.character['saske/须左'].append_update_function(self.xuzuo_update)
         self.saske_xuzuo.character['saske/须左'].append_end_update_function(self.saske_xuzuo.remove_current_sprite_from_sprite_group)
+        self.play_sound('须左.wav')
         
     def xie_lun_yan(self):
         self.saske_xielunyan = SaskeStyle(self.sprite_group, situation_flag = 4)
+        self.play_sound('写轮眼.wav')
         self.saske_xielunyan.set_left_padding(self.saske_style.get_left_padding() - 40)
         self.saske_xielunyan.set_top_padding(self.saske_style.get_top_padding() - 40)
         self.saske_xielunyan.character['saske/写轮眼'].append_end_update_function(self.xu_zuo)
@@ -505,6 +524,12 @@ class Controller():
     #对手在哪里，佩恩的球就在哪里形成
     #佩恩特效###############################################################################################
     
+    def test_cast_sound(self):
+        self.play_sound_queue('爆炸.wav')
+        self.play_sound_queue('射出箭.wav')
+        self.play_sound_queue('搭箭.wav')
+        
+    
     def update_screen_with_white(self):
         self.screen.fill(Color.WHITE)
     
@@ -512,17 +537,18 @@ class Controller():
         if self.key_controller.key_m:
             self.chu_fa_fen_shen()
         elif self.key_controller.key_w:
-            #self.xie_lun_yan()
+            self.xie_lun_yan()
             #self.sa_pu_song_zang()
             #self.sa_fu_jiu()
             #self.shui_long_dan_zhi_shu()
             #self.shen_wei()
             #self.change_to_tong_ling()
-            self.release_di_bao_tian_xing()
+            #self.release_di_bao_tian_xing()
         elif self.key_controller.key_a:
             #self.add_wo_ai_luo()
             #self.add_zi_lai_ye()
-            self.add_pain()
+            #self.add_pain()
+            self.test_cast_sound()
         elif self.key_controller.key_s:
             #self.change_to_status('naruto/idle')
             #self.wo_ai_luo_sa_fu_jiu.character['woailuo/砂缚柩'].clear_update_function()
@@ -552,15 +578,21 @@ class Controller():
         self.saske_style.add_to_sprite_group()
         self.saske_style.set_key_controller(self.key_controller)
     
+    def cast_dao_di_voice(self, image_index):
+        if image_index == 7:
+            self.play_sound('倒地上.wav')
+    
     def saske_be_hit_far_away(self):
         self.saske_style.change_to_status('倒在地上')
         self.current_attack_player = 'saske'
         self.current_attacked_player = 'naruto'
+        self.saske_style.character['saske/倒在地上'].append_update_function(self.cast_dao_di_voice)
     
     def naruto_be_hit_far_away(self):
         self.naruto_style.change_to_status('death')
         self.current_attack_player = 'naruto'
         self.current_attacked_player = 'saske'
+        self.naruto_style.character['naruto/death'].append_update_function(self.cast_dao_di_voice)
         
     def naruto_be_houyang(self):
         self.naruto_style.change_to_status('后仰')
@@ -580,12 +612,23 @@ class Controller():
             #self.saske_style.change_to_status('站起来')
             self.handle_key_event()
             #return True
-            if self.insert_action_num == 0 and self.sleep(3000):
+            if self.insert_action_num == 0 and self.sleep(1000):
                 #self.saske_style.change_to_status('站起来')
                 self.naruto_style.change_to_status('run')
                 self.saske_style.change_to_status('run')
 
         return False
+    
+    def play_yong_tou_da(self, image_index):  #其实这些声音应该挂在具体的玩家身上
+        if image_index == 3:
+            self.play_sound('打中别人.wav')
+    
+    def play_hui_tui_by_index(self, image_index):
+        if image_index == 7:
+            self.play_sound('打中别人.wav')
+    
+    def play_hui_tui(self):
+        self.play_sound('挥拳.wav')
     
     def naruto_attack_saske(self):
         #在下面添加判断的逻辑，是在跑还是在干什么，根据逻辑确定两者之间的位置以及是否要切换
@@ -593,13 +636,33 @@ class Controller():
             #首先跑一下看看是不是idle3秒之后开始相对跑近     然后下面需要判断一下是不是距离足够进了
             #print('padding:' + str(self.saske_style.get_left_padding() - self.naruto_style.get_left_padding()))
             if(self.saske_style.get_left_padding() - self.naruto_style.get_left_padding()) < 84:
+                self.play_sound('挥拳.wav')
                 self.naruto_style.change_to_status('挥拳')
                 self.naruto_style.append_end_update_huiquan(self.saske_style.change_to_houyang())
                 self.naruto_style.append_end_update_yongtouda(self.saske_style.change_to_houyang())
                 self.naruto_style.append_end_update_fanjiaoti(self.saske_be_hit_far_away)
                 
+                self.naruto_style.character["naruto/用头打"].append_update_function(self.play_yong_tou_da)
+                
+                self.naruto_style.character["naruto/反脚踢"].append_update_function(self.play_hui_tui_by_index)
         #后仰的时候，先顺着波，然后倒着播，中途被打了再顺着播，然后倒着播，播完了就变成idle了
     
+    def cast_dao_kan(self, image_index):
+        if image_index == 3:
+            self.play_sound('刀砍.wav')
+            
+    def cast_hui_quan(self, image_index):
+        if image_index == 3:
+            self.play_sound('打中别人.wav')
+            
+    def cast_liang_dao(self, image_index):
+        if image_index == 2 or image_index == 5:
+            self.play_sound('刀砍.wav')
+            
+    def cast_xiang_shang_yi_jiao(self, image_index):
+        if image_index == 3:
+            self.play_sound('打得重.wav')
+            
     def saske_attack_naruto(self):
         if not self.idle_begin_run() and self.naruto_style.status == 'run' and self.saske_style.status == 'run':
             #print('padding:' + str(self.saske_style.get_left_padding() - self.naruto_style.get_left_padding()))
@@ -610,7 +673,10 @@ class Controller():
                 self.saske_style.append_end_update_huiquan(self.naruto_be_houyang)
                 self.saske_style.append_update_huiliangdao(self.naruto_be_houyang_with_image_index)
                 self.saske_style.append_end_update_up_ti(self.naruto_be_hit_far_away)
-        
+                self.saske_style.character['saske/挥刀'].append_update_function(self.cast_dao_kan)
+                self.saske_style.character['saske/挥拳'].append_update_function(self.cast_hui_quan)
+                self.saske_style.character['saske/挥两刀'].append_update_function(self.cast_liang_dao)
+                self.saske_style.character['saske/向上一脚'].append_update_function(self.cast_xiang_shang_yi_jiao)
     
     def start_to_update(self):
         if self.naruto_style.status == 'idle' and self.saske_style.status == 'idle':
